814d3a4a0357b9f87e1b12ae19ebdc03b99c6f32
Hasan Rakib <hm.hasan@gmail.com>
2025-12-08 15:25:22 +0600

    Modified User.php so the dashboard is accessible

--- app/Models/User.php
+++ app/Models/User.php
@@ -63,9 +63,9 @@ class User extends Authenticatable implements FilamentUser
 
     public function canAccessPanel(Panel $panel): bool
     {
-        // return str_ends_with($this->email, '@yourdomain.com') 
-        // && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@karkhanabd.com') 
+        && $this->hasVerifiedEmail();
 
-        return $this->email == 'admin@karkhana.com';
+        //return $this->email == 'admin@karkhana.com';
     }
 }
